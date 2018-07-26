f72f73fcc88d7ace0e507088ae7f1a909e8e5873
Quinn Reilly <Quinn.Reilly@hotmail.com>
2018-07-26 23:41:12 +0100

    0.01.1

--- inc/object.h
+++ inc/object.h
@@ -4,39 +4,36 @@
 class object
 {
 private:
-    
+
     sf::FloatRect objectSize, objectPos;
     sf::Color objectColor;
     std::string objectSprite;
     bool isObjectGravityOn;
     int objectGravity, objectSpeed;
-    
+
 public:
-    
+
     void setObjectSize(FloatRect objectSz);
     sf::FloatRect getObjectSize();
-    
+
     void setObjectPos(FloatRect objectPs);
     sf::FloatRect getObjectPos();
-    
+
     void setObjectColor(sf::Color objectClr);
     sf::Color getObjectColor();
-    
+
     void setObjectSprite(std::string objectSpr);
     std::string getObjectSprite();
-    
+
     void setIsObjectGravityOn(int isObjectGravOn);
     int getIsObjectGravityOn();
-    
+
     void setObjectGravity(int objectGrav);
     int getObjectGravity();
-    
+
     void setObjectSpeed(int objectSpd);
     int getObjectSpeed();
-    
-    void move();
-    void gravity();
-    
+
 };
 
 #endif
